9f42211e98d897bd07085db73e82b3e3e519c6cb
adjsium <leonardowukamting@gmail.com>
2023-06-07 02:50:04 -0400

    fix api leak

--- src/geoDBapi.js
+++ src/geoDBapi.js
@@ -1,8 +1,10 @@
 export const GEO_API_url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '65d316e7bamsh1d25e522b256079p1ed6c1jsna5fdb3dd0518',
+		'X-RapidAPI-Key': process.env.GEO_DB_API_KEY,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
