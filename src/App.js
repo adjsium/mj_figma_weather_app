10c39cf016b52c8765be100b2ac16d5c44bd3a4b
adjsium <leonardowukamting@gmail.com>
2023-06-07 02:01:54 -0400

    search bar implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-
+import React from "react";
+import Search from './component/Search';
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+  const string = "Hello World";
   return (
     <div className="App">
-    
+    <Search onSearchChange={handleOnSearchChange}/>
+    <p>{string}</p>
     </div>
   );
 }
